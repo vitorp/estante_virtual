94037fa11444b0081e28a05d15dc0d7c2126b01e
Renan Reboredo <renanrreboredo@gmail.com>
2018-05-29 15:36:28 -0300

    Add validation of presence to all fields of books

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,10 @@ class Book < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates_uniqueness_of :title
+
+  validates_presence_of :title
+  validates_presence_of :author
+  validates_presence_of :code
+  validates_presence_of :publication_date
+  validates_presence_of :genre
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,4 +4,10 @@ require "rails_helper"
 
 RSpec.describe Book, type: :model do
   it { should validate_uniqueness_of :title }
+
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :author }
+  it { should validate_presence_of :publication_date }
+  it { should validate_presence_of :code }
+  it { should validate_presence_of :genre }
 end
