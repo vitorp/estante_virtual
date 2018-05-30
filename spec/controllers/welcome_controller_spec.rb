e7c4ed643288263d4f6b718c4293dcffc9eb8235
Vitor Pontes <vitormax2005@hotmail.com>
2018-05-30 03:19:15 +0000

    Added login page and specs.

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,17 +3,41 @@
 require "rails_helper"
 
 RSpec.describe WelcomeController, type: :controller do
-  let(:login_data) { {nickname: "Tuts", password: "senha"} }
+  let(:login_data) { {login: {nickname: "Tuts", password: "senha"}} }
 
   describe "POST #login" do
+    subject { post :login, params: login_data }
     context "when login is valid" do
-      let(:user) {
-        User.create({ name: "Mateus", nickname: login_data.nickname , password: login_data.password, phone: "999999999" })
+      let!(:user) {
+        User.create(name:     "Mateus",
+                    nickname: login_data[:login][:nickname],
+                    password: login_data[:login][:password],
+                    phone:    "999999999")
       }
 
       it "defines current user" do
-        post :login, params: login_data
+        subject
         expect(controller.current_user).to eq(user)
+      end
+
+      it "redirects to user home" do
+        expect(subject).to redirect_to(user_home_path)
+      end
+    end
+
+    context "when login is invalid" do
+      it "doesn't define current user" do
+        subject
+        expect(controller.current_user).to be_nil
+      end
+
+      it "renders to login page" do
+        expect(subject).to render_template("welcome/home")
+      end
+
+      it "sets an error message" do
+        subject
+        expect(flash[:error]).to include("Login inválido")
       end
     end
   end
