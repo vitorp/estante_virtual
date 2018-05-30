9249aa3155930d1d2d2eb71a1ee2fa9cff421c39
Renan Reboredo <renanrreboredo@gmail.com>
2018-05-29 21:48:14 -0300

    Add validation of belongs to and has many for books

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,6 +3,9 @@
 require "rails_helper"
 
 RSpec.describe Book, type: :model do
+  it { should belong_to :user }
+  it { should have_many :reviews }
+
   it { should validate_uniqueness_of :title }
 
   it { should validate_presence_of :title }
