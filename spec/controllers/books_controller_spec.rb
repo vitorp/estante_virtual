acb9e58512ceb56d56292b0ece4a73db34903a2a
vitor pontes <vitormax2005@hotmail.com>
2018-06-18 17:05:44 -0300

    Add new redirect spec to books controller.

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -20,11 +20,16 @@ RSpec.describe BooksController, type: :controller do
   end
 
   describe "POST #create" do
+    subject { post :create, params: book_params }
+
     it "creates a new book" do
       controller.login_user(user)
-      expect {
-        post :create, params: book_params
-      }.to change(Book, :count).by(1)
+      expect { subject }.to change(Book, :count).by(1)
+    end
+
+    it "redirects to books show" do
+      controller.login_user(user)
+      expect(subject).to redirect_to(book_path(assigns(:book)))
     end
   end
 end
