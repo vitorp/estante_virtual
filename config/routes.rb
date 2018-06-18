1b6b5ce1e5a9ba4d22775f9b3accd226ea9c1717
vitor pontes <vitormax2005@hotmail.com>
2018-06-18 20:23:26 -0300

    Add book details front and shelf.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   root to: "welcome#home", as: "login_form"
 
   resources :books, only: [:create, :new, :show]
+  resources :reviews, only: [:new, :create, :destroy]
 
   post "/users", to: "user#create", as: "users"
   get "/users/home", to: "user#home", as: "user_home"
   get "/users/new", to: "user#new", as: "new_user"
+  get "/users/:user_id", to: "user#show", as: "user"
   post "/login", to: "welcome#login", as: "login"
 
   get "/shelf", to: "books#shelf", as: "shelf"
