3f3af7b9c3b431b1abf74e5a3e949528b1690752
vitor pontes <vitormax2005@hotmail.com>
2018-06-19 17:22:49 -0300

    Add book setup and remove trade specs

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -12,6 +12,15 @@ RSpec.describe BooksController, type: :controller do
             genre:            "genero"}}
   }
 
+  let(:book) {
+    Book.create(author:           "Renan",
+                title:            "Titulo",
+                publication_date: Date.new(2015, 11, 23),
+                code:             "codigo",
+                genre:            "genero",
+                user_id:          user.id)
+  }
+
   let(:user) { User.create(name: "User", nickname: "user_nick", password: "senha", phone: "123456789") }
 
   describe "GET #new" do
@@ -42,5 +51,23 @@ RSpec.describe BooksController, type: :controller do
     end
 
     it { is_expected.to have_http_status(:success) }
+  end
+
+  describe "GET #setup_trade" do
+    subject { get :setup_trade, params: {id: book.id} }
+
+    it "turns book tradable" do
+      subject
+      expect(book.reload).to be_tradable
+    end
+  end
+
+  describe "GET #remove_trade" do
+    subject { get :remove_trade, params: {id: book.id} }
+
+    it "turns book untradable" do
+      subject
+      expect(book.reload).to_not be_tradable
+    end
   end
 end
